39b0679694e144dcdc938d19e4ba4d3e8da21ac7
Alex Corvi <alex@arrayy.com>
2017-03-15 07:40:26 +0300

    1.0.2

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,20 @@
-import {Fin} from "finnlp";
-import {negation} from "../src/index";
-Fin.addDetector(negation);
-//console.log(Fin.detectors);
-console.log((Fin.run("I do not only have any work left") as any).negation());
+import * as Fin from "finnlp";
+import "../src/index";
+
+var negation = new Fin.Run("no work left").negation();
+console.log(negation);
+var negation = new Fin.Run("I didn't leave any trace on the scene").negation();
+console.log(negation);
+var negation = new Fin.Run("That's not a good excuse").negation();
+console.log(negation);
+var negation = new Fin.Run("Well, he wasn't here only for few hours").negation();
+console.log(negation);
+var negation = new Fin.Run("You shouldn't run that fast").negation();
+console.log(negation);
+var negation = new Fin.Run("Don't send it just yet").negation();
+console.log(negation);
+var negation = new Fin.Run("That's really unlike you").negation();
+console.log(negation);
+var negation = new Fin.Run("There was zero initial statements.").negation();
+console.log(negation);
+
